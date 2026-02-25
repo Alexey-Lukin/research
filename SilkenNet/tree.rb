0278917fff6a5653725c51fc94f8cd22df1a2761
Alexey Lukin <realvirtuozzz@gmail.com>
2026-02-25 22:12:57 +0200

    Add maintenance_records association to tree model

--- SilkenNet/tree.rb
+++ SilkenNet/tree.rb
@@ -5,9 +5,10 @@ class Tree < ApplicationRecord
   belongs_to :tree_species
   # Гаманець знищується разом з деревом, якщо воно вмирає/спилюється
   has_one :wallet, dependent: :destroy
+  has_one :device_calibration, dependent: :destroy
   # Історія пульсу знищується (або можна залишити nullify для архіву)
   has_many :telemetry_logs, dependent: :destroy
-  has_one :device_calibration, dependent: :destroy
+  has_many :maintenance_records, as: :maintainable
 
   # did - це 32-бітний хеш, згенерований в main.c з UID STM32 та шуму кристала
   validates :did, presence: true, uniqueness: true
